e9443caeb96287be224eed60d2894db5af78d3fb
Valeria Sanchez <valef8578@gmail.com>
2024-09-26 16:48:54 -0500

    Se crea el proyecto con los ejemplos de los operadores

---
+++ operadoresProject/OperadoresAtitmeticos.py
@@ -0,0 +1,23 @@
+#Ejemplos utilizando operadores aritmeticos
+#numero1=int(input("Ingrese el numero 1: "))
+#numero2=int(input("Ingrese el numero 2: "))
+
+#suma= numero1+ numero2
+#print("La suma es: " ,str(suma))
+
+#Artimeticos
+numero3=int(input("Ingrese el numero: "))
+numero4=int(input("Ingrese el numero: "))
+
+suma1= numero3 + numero4
+resta= numero3 - numero4
+division= numero3 / numero4
+multiplicacion = numero3 * numero4
+modulo= numero3 % numero4
+divisionEntera= numero3 // numero4
+
+print(f"La suma de {numero3} y {numero4} es: ", str(suma1))
+print(f"La resta de {numero3} y {numero4} es: ", str(resta))
+print(f"La divison de {numero3} y {numero4} es: ", str(division))
+print(f"La multiplicacion de {numero3} y {numero4} es: ", str(multiplicacion))
+print(f"La division entera de {numero3} y {numero4} es: ", str(divisionEntera))
